551d832ec009b097967aecd8f8ee505b5a7650ba
Seung-han LEE <beonit@gmail.com>
2018-01-09 14:41:30 +0900

    Update StringResponse.java

--- src/main/java/com/github/paweladamski/httpclientmock/action/StringResponse.java
+++ src/main/java/com/github/paweladamski/httpclientmock/action/StringResponse.java
@@ -10,17 +10,25 @@ import java.nio.charset.Charset;
 
 public class StringResponse implements Action {
 
+    private final int statusCode;
     private final String response;
     private final Charset charset;
 
     public StringResponse(String response, Charset charset) {
+        this.statusCode = 200;
+        this.response = response;
+        this.charset = charset;
+    }
+
+    public StringResponse(int statusCode, String response, Charset charset) {
+        this.statusCode = statusCode;
         this.response = response;
         this.charset = charset;
     }
 
     @Override
     public HttpResponse getResponse(Request request) {
-        BasicHttpResponse response = new BasicHttpResponse(new ProtocolVersion("http", 1, 1), 200, "ok");
+        BasicHttpResponse response = new BasicHttpResponse(new ProtocolVersion("http", 1, 1), statusCode, "ok");
         response.setEntity(new StringEntity(this.response, this.charset));
         return response;
     }
