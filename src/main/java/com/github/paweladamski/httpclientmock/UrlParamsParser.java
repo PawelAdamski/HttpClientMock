7191f5168f7c9817a739825728284eac437c63d8
padamski <padamski@pgs-soft.com>
2020-06-23 21:54:46 +0200

    Simplify UrlParamsParser

--- src/main/java/com/github/paweladamski/httpclientmock/UrlParamsParser.java
+++ src/main/java/com/github/paweladamski/httpclientmock/UrlParamsParser.java
@@ -2,25 +2,23 @@ package com.github.paweladamski.httpclientmock;
 
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URLEncodedUtils;
 
-public class UrlParamsParser extends ArrayList<NameValuePair> {
+public class UrlParamsParser {
 
   public List<NameValuePair> parse(String query) {
     return parse(query, StandardCharsets.UTF_8);
   }
 
   public List<NameValuePair> parse(String query, Charset charset) {
-    List<NameValuePair> params = new UrlParamsParser();
-
     if (query != null) {
-      params.addAll(URLEncodedUtils.parse(query, charset));
+      return URLEncodedUtils.parse(query, charset);
+    } else {
+      return Collections.emptyList();
     }
-
-    return params;
   }
 
 }
